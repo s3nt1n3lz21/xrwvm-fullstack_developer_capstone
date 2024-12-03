c8d28c2bf8f575799d0ea407cd3524e8e1ead02c
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2024-12-03 11:31:08 -0500

    applied flake8 python linting fixes to ignore some long lines

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,9 +30,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    'https://s3nt1n3lz18-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://s3nt1n3lz18-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'] # noqa: E501
 CSRF_TRUSTED_ORIGINS = [
-    'https://s3nt1n3lz18-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://s3nt1n3lz18-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'] # noqa: E501
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -97,7 +97,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator', # noqa: E501
     },
     {
         'NAME':

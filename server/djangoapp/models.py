087427f4b82eaa8aa7886ef49970d9ef33487e7e
s3nt1n3lz21 <s3nt1n3lz18@gmail.com>
2024-12-03 11:17:46 -0500

    applied python linting fixes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -31,8 +31,10 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -42,10 +44,10 @@ class CarModel(models.Model):
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
     year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
+                               validators=[
+                                   MaxValueValidator(2023),
+                                   MinValueValidator(2015)
+                               ])
     # dealerId = models.IntegerField(default=1)
 
     def __str__(self):
